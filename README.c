6773fe9549ce06de8c4b6404379c2f2d4ce6e79d
mahaamu <mahalakshmi3901@gmail.com>
2016-07-08 14:42:51 +0530

    Update and rename README.md to README.c
    finish

---
+++ README.c
@@ -0,0 +1,20 @@
+# c
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b,c;
+printf("enter the no's");
+scanf("%d%d%d",&a&b&c);
+if(a>b)
+{
+printf("a is gtreater");
+else
+if(b>c)
+{
+printf("b is greater");
+else
+printf("c is greater");
+}
+}
+}
